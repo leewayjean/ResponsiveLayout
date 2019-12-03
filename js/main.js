afc0f079496570ceabcd319514d7efb5c2539db9
Lee <39004900+leewayjean@users.noreply.github.com>
2019-12-03 21:20:26 +0800

    添加切换播放

--- js/main.js
+++ js/main.js
@@ -2,18 +2,38 @@
 let reviewList = document.querySelectorAll(".review_item");
 // 播放器
 let videoPlayer = document.querySelector("video");
-// 往期回顾视频列表src
-let videoList = ["./video/Macbook.mp4","./video/iPhone.mp4","./video/iPad.mp4"];
 // 播放按钮
 let playBtn = document.querySelector(".play_btn");
+// 视频标题
+let videoTitle = document.querySelector(".video").querySelector(".title");
+
+
+
+// 往期回顾接口数据
+let videoList = [
+    {
+        title: "八周交易计划2 《我的信仰》",
+        src: "./video/Macbook.mp4"
+    },
+    {
+        title: "第八集：巅峰之战！谁是黑马？",
+        src: "./video/iPhone.mp4"
+    },
+    {
+        title: "巨债承压动真格，赚钱才是真本事！",
+        src: "./video/iPad.mp4"
+    }
+];
+
 
 // 切换视频
-for(let i = 0 ; i < reviewList.length; i++){
-    reviewList[i].addEventListener("click",() => {
+for (let i = 0; i < reviewList.length; i++) {
+    reviewList[i].addEventListener("click", () => {
         // 切换视频
-        videoPlayer.src = videoList[i];
-        // 如果按钮隐藏了就显示出来
-        if(playBtn.style.display == "none"){
+        videoPlayer.src = videoList[i].src;
+        videoTitle.innerText = videoList[i].title;
+            // 如果按钮隐藏了就显示出来
+            if(playBtn.style.display == "none"){
             playBtn.style.display = "block";
         }
 
@@ -21,13 +41,13 @@ for(let i = 0 ; i < reviewList.length; i++){
 }
 
 // 播放、暂停
-playBtn.addEventListener("click",() => {
+playBtn.addEventListener("click", () => {
     videoPlayer.play();
     playBtn.style.display = "none";
 })
-videoPlayer.addEventListener("click",() => {
+videoPlayer.addEventListener("click", () => {
     videoPlayer.pause();
-    if(playBtn.style.display == "none"){
+    if (playBtn.style.display == "none") {
         playBtn.style.display = "block";
     }
 })
